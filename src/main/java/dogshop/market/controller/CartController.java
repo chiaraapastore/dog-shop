a39db83853fcc93df6795487fb33a93b4e93ad6f
chiaraapastore <127127969+chiaraapastore@users.noreply.github.com>
2024-11-10 13:15:32 +0100

    Update CartController.java
    fix colntroller cart

--- src/main/java/dogshop/market/controller/CartController.java
+++ src/main/java/dogshop/market/controller/CartController.java
@@ -2,9 +2,7 @@ package dogshop.market.controller;
 
 import dogshop.market.entity.Cart;
 import dogshop.market.entity.CartProductRequest;
-import dogshop.market.entity.CustomerOrder;
 import dogshop.market.service.CartService;
-import dogshop.market.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,32 +15,22 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
-    @Autowired
-    private OrderService orderService;
-
     @PostMapping("/create")
     public ResponseEntity<Cart> createCart(@RequestParam Long userId) {
         Cart newCart = cartService.createCart(userId);
         return ResponseEntity.status(HttpStatus.CREATED).body(newCart);
     }
 
-    @GetMapping("/user/{userId}")
-    public ResponseEntity<Cart> getCartByUserId(@PathVariable Long userId) {
-        Cart cart = cartService.getCartByUserId(userId);
+
+    @GetMapping("/user/{userId}/with-products")
+    public ResponseEntity<Cart> getCartWithProductsByUserId() {
+        Cart cart = cartService.getCartByUser();
         return ResponseEntity.ok(cart);
     }
 
-    @PostMapping("/add")
+    @PostMapping("/addProductToCart")
     public ResponseEntity<Cart> addProductToCart(@RequestBody CartProductRequest request) {
-        System.out.println("Ricevuto richiesta: cartId=" + request.getCartId() + ", productId=" + request.getProductId() + ", quantity=" + request.getQuantity());
-        Cart updatedCart = cartService.addProductToCart(request.getCartId(), request.getProductId(), request.getQuantity());
+        Cart updatedCart = cartService.addProductToCart(request.getUserId(), request.getProductId(), request.getQuantity());
         return ResponseEntity.status(HttpStatus.CREATED).body(updatedCart);
-    }
-
-
-    @PostMapping("/checkout/{cartId}")
-    public ResponseEntity<CustomerOrder> checkout(@PathVariable Long cartId) {
-        CustomerOrder newOrder = orderService.createOrderFromCart(cartId);
-        return ResponseEntity.status(HttpStatus.CREATED).body(newOrder);
     }
 }
