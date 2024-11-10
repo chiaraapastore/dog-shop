92f9cabb41cb720fa5a4368ced912d87312d7e31
chiaraapastore <127127969+chiaraapastore@users.noreply.github.com>
2024-11-10 13:14:25 +0100

    Update CartService.java
    fix sul metod addProductToCart

--- src/main/java/dogshop/market/service/CartService.java
+++ src/main/java/dogshop/market/service/CartService.java
@@ -16,26 +16,46 @@ public class CartService {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private AuthenticationService authenticationService;
+    @Autowired
+    private UtenteShopRepository utenteShopRepository;
+
+
+
     public Cart getCartByUserId(Long userId) {
         return cartRepository.findCartWithProductsByUserId(userId)
                 .orElseGet(() -> createCart(userId));
     }
 
-    public Cart createCart(Long userId) {
-        Cart cart = new Cart();
-        cart.setUserId(userId);
-        return cartRepository.save(cart);
+   public Cart getCartByUser() {
+        UtenteShop currentUser = utenteShopRepository.findByUsername(authenticationService.getUsername());
+        return cartRepository.findCartWithProductsByUtenteShop(currentUser);
     }
 
-    public Cart addProductToCart(Long cartId, Long productId, int quantity) {
-        Cart cart = cartRepository.findById(cartId)
-                .orElseThrow(() -> new RuntimeException("Carrello non trovato"));
+
+    public Cart addProductToCart(Long productId, int quantity) {
+        //con questo ti recuperi l'utente corrente che ha fatto login
+        UtenteShop utenteShop = utenteShopRepository.findByUsername(authenticationService.getUsername())
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        // qui prendi il prodotto tramite l'id
         Product product = productRepository.findById(productId)
-                .orElseThrow(() -> new RuntimeException("Prodotto non trovato"));
+                .orElseThrow(() -> new IllegalArgumentException("Product not found"));
 
-        cart.addProduct(product, quantity);
+        // Qua o trovo il carrello se è già esistente o ne creo uno nuovo se è il primo
+        Cart cart = cartRepository.findByUtenteShop(utenteShop).orElseGet(() -> {
+            Cart newCart = new Cart();
+            newCart.setUtenteShop(utenteShop);
+            return newCart;
+        });
+
+        // qua aggiungo il prodotto al carrello
+        product.setAvailableQuantity(quantity);
+        cart.getProducts().add(product);
+
+        // qua salvo il carrello
         return cartRepository.save(cart);
     }
-
 
 }
